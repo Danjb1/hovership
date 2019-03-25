b7c7acfe5c5b850b26600392581a3802a9972dda
esummers1 <eddie.summers1@gmail.com>
2019-03-25 20:04:30 +0000

    Add windmill blades & rotation

---
+++ hovership/Assets/Scripts/WindmillBladesController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindmillBladesController : MonoBehaviour {
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Script Properties
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * The rotation speed of the blades, in degrees per second.
+     */
+    public float rotationSpeed;
+
+    ///////////////////////////////////////////////////////////////////////////
+    // WindmillBladesController
+    ///////////////////////////////////////////////////////////////////////////
+
+    void Update () {
+
+        // Do nothing if we are celebrating a level completion
+        if (StateManager.Instance.GetState() == GameState.CELEBRATING) {
+            return;
+        }
+
+        Vector3 rotation = Vector3.up * rotationSpeed;
+        transform.Rotate(
+                rotation.x * Time.deltaTime,
+                rotation.y * Time.deltaTime,
+                rotation.z * Time.deltaTime
+        );
+    }
+
+}
