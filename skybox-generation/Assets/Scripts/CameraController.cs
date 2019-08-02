6e3619ca46792b75371997a7b7443a26e9a55527
Dan Bryce <admin@danjb.com>
2019-08-02 08:42:50 +0100

    Fix Skybox seams

--- skybox-generation/Assets/Scripts/CameraController.cs
+++ skybox-generation/Assets/Scripts/CameraController.cs
@@ -1,64 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEditor;
 using UnityEngine;
-using UnityEngine.Windows;
 
+// Based on:
+// https://medium.com/acrossthegalaxy/making-skyboxes-directly-in-unity-7250fe90c632
 public class CameraController : MonoBehaviour {
 
-    public Camera front;
-    public Camera right;
-    public Camera back;
-    public Camera left;
-    public Camera up;
-    public Camera down;
-
-    void Start() {
-        DisableAll();
-
-        front.enabled = true;
-    }
-
-    void DisableAll() {
-        front.enabled = false;
-        right.enabled = false;
-        back.enabled = false;
-        left.enabled = false;
-        up.enabled = false;
-        down.enabled = false;
-    }
+    const int TEXTURE_SIZE = 1024;
 
     void Update() {
-        if (Input.GetMouseButton(0) || Input.GetKey("enter")) {
-            Capture(front, "front.png");
-            Capture(right, "right.png");
-            Capture(back, "back.png");
-            Capture(left, "left.png");
-            Capture(up, "up.png");
-            Capture(down, "down.png");
-            Application.Quit();
+        if (Input.anyKey) {
+            Capture();
         }
     }
 
-    // Based on:
-    // https://forum.unity.com/threads/how-to-save-manually-save-a-png-of-a-camera-view.506269/
-    //
-    void Capture(Camera camera, string filename) {
+    void Capture() {
+        Cubemap cubemap = new Cubemap(TEXTURE_SIZE, TextureFormat.ARGB32, true);
+        cubemap.name = "Skybox";
+        Camera camera = GetComponent<Camera>();
+        camera.RenderToCubemap(cubemap);
 
-        // Render to the Camera's texture
-        RenderTexture prev = RenderTexture.active;
-        RenderTexture.active = camera.targetTexture;
-        camera.Render();
-
-        // Save texture to image
-        Texture2D Image = new Texture2D(camera.targetTexture.width, camera.targetTexture.height);
-        Image.ReadPixels(new Rect(0, 0, camera.targetTexture.width, camera.targetTexture.height), 0, 0);
-        Image.Apply();
-        var bytes = Image.EncodeToPNG();
-        File.WriteAllBytes(filename, bytes);
-
-        // Clean up
-        Destroy(Image);
-        RenderTexture.active = prev;
+        AssetDatabase.CreateAsset(
+          cubemap,
+          "Assets/Textures/Skybox.cubemap"
+        );
     }
 
 }
